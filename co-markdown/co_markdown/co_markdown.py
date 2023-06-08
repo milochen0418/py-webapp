a19269896288d11fe858cc327e474da19fd15451
milochen0418 <milo_chen@mail2000.com.tw>
2023-06-08 11:45:36 +0800

    real-time editing for markdown

--- co-markdown/co_markdown/co_markdown.py
+++ co-markdown/co_markdown/co_markdown.py
@@ -5,6 +5,7 @@ import pynecone as pc
 
 class State(pc.State):
     """The app state."""
+    mdtext: str = "Type markdown in left"
     pass
 
 
@@ -14,13 +15,9 @@ def index() -> pc.Component:
             pc.heading("Collaborative Markdown", font_size="2em", w="100%"),
             pc.hstack(
                 pc.vstack(
-                    pc.markdown("""
-                    # Markdown One
-                    ## Document Test
-                    - This is a book
-                    - This is book 2
-                    - This is item 3
-                    """
+                    pc.text_area(
+                        on_change=State.set_mdtext,
+                        default_value="# Markdown Two  ## Document Test ![](https://user-images.githubusercontent.com/12568287/239189051-fb2ddd86-8bc4-49c8-8bbb-f7be3040b1fe.png)",
                     ),
                     align_items="left",
                     bg="#ededed",
@@ -31,18 +28,7 @@ def index() -> pc.Component:
                     w="100%",
                 ),
                 pc.vstack(
-                    pc.markdown("""
-                    # Markdown Two
-                    ## Document Test
-                    - This is a book
-                    - This is book 2
-                    - This is item 3
-                    ## Document Test 2
-                    - This is a book A
-                    - This is book B
-                    - This is item C
-                    """
-                    ),
+                    pc.markdown(State.mdtext),
                     align_items="left",
                     bg="#ededed",
                     margin="0em",
@@ -53,7 +39,6 @@ def index() -> pc.Component:
                 ),
                 spacing="0.5em",
                 w="100%",
-                
             ),
             spacing="1.5em",
             font_size="2em",
@@ -62,8 +47,6 @@ def index() -> pc.Component:
         ),
         w="100%",
     )
-
-
 # Add state and page to the app.
 app = pc.App(state=State)
 app.add_page(index)
